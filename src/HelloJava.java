e61e835fb626b6591c8e401b99b45483da373c03
bigth21 <taekhyeon.nam@gmail.com>
2023-11-30 14:12:52 +0900

    Hello world

---
+++ src/HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava {
+
+    /*
+    Multi line comment
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Java"); // Single line comment
+    }
+}
