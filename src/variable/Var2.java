37db26e8e105c98bcf034101635c4d4633cc0c87
bigth21 <taekhyeon.nam@gmail.com>
2023-11-30 14:58:47 +0900

    Variable

---
+++ src/variable/Var2.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var2 {
+
+    public static void main(String[] args) {
+        int number = 20;
+        System.out.println(number);
+        System.out.println(number);
+        System.out.println(number);
+    }
+}
