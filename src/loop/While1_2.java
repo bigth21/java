6ed0f4ad18188358f801d159316d40e3eb3e4d26
bigth21 <taekhyeon.nam@gmail.com>
2023-11-30 21:38:22 +0900

    Loop

---
+++ src/loop/While1_2.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class While1_2 {
+
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 0) {
+            count++;
+            System.out.println("count = " + count);
+        }
+    }
+}
