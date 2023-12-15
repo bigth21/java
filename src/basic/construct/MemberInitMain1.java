bd1ab810c0ab4d1d34b550ac26984dd2cbe9076c
bigth21 <taekhyeon.nam@gmail.com>
2023-12-15 23:25:26 +0900

    Constructor

---
+++ src/basic/construct/MemberInitMain1.java
@@ -0,0 +1,17 @@
+package basic.construct;
+
+public class MemberInitMain1 {
+    public static void main(String[] args) {
+        MemberInit member1 = new MemberInit();
+        member1.initMember("user1", 15, 90);
+
+        MemberInit member2 = new MemberInit();
+        member2.initMember("user2", 16, 80);
+
+        MemberInit[] members = {member1, member2};
+
+        for (MemberInit member : members) {
+            System.out.printf("name:%s age:%d grade:%d\n", member.name, member.age, member.grade);
+        }
+    }
+}
