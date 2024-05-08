39b181e2321b4649a1a59ef6f5796837372d22e7
bigth21 <taekhyeon.nam@gmail.com>
2024-05-08 23:12:20 +0900

    Immutable Obj

---
+++ src/lecture/mid1/lang/immutable/address/ImmutableAddress.java
@@ -0,0 +1,20 @@
+package lecture.mid1.lang.immutable.address;
+
+public class ImmutableAddress {
+    private String value;
+
+    public ImmutableAddress(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "ImmutableAddress{" +
+                "value='" + value + '\'' +
+                '}';
+    }
+}
