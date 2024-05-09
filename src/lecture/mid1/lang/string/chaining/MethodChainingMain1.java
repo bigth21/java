5204b3aa7bd1cd5d4179d0ff810e96c2bed1042a
bigth21 <taekhyeon.nam@gmail.com>
2024-05-10 00:28:37 +0900

    String

---
+++ src/lecture/mid1/lang/string/chaining/MethodChainingMain1.java
@@ -0,0 +1,12 @@
+package lecture.mid1.lang.string.chaining;
+
+public class MethodChainingMain1 {
+    public static void main(String[] args) {
+        int value = new ValueAdder()
+                .add(1)
+                .add(2)
+                .add(3)
+                .getValue();
+        System.out.println("value = " + value);
+    }
+}
---
+++ src/lecture/mid1/lang/string/method/StringChangeMain1.java
@@ -0,0 +1,18 @@
+package lecture.mid1.lang.string.method;
+
+public class StringChangeMain1 {
+    public static void main(String[] args) {
+        String str = "Hello, Java! Welcome to Java";
+
+        System.out.println("str.substring(7) = " + str.substring(7));
+        System.out.println("str.substring(7) = " + str.substring(7, 12));
+        System.out.println("str.concat(\"!!!\") = " + str.concat("!!!"));
+
+        System.out.println("str.replace(\"Java\", \"World\") = " + str.replace("Java", "World")); // Replace all
+        System.out.println("str.replace('o', '') = " + str.replace('o', 'a')); // Replace all
+
+        System.out.println("str.replaceFirst(\"Java\", \"World\") = " + str.replaceFirst("Java", "World")); //Replace first. Parameter type is regex.
+        System.out.println("str.replaceAll(\"Java\", \"World\") = " + str.replaceAll("Java", "World")); //Replace all. Parameter type is regex.
+
+    }
+}
