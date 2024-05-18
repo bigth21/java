5e259d0ec1ad36f94855c0deb1dd59ea3113b935
bigth21 <taekhyeon.nam@gmail.com>
2024-05-18 17:53:45 +0900

    Time

---
+++ src/lecture/mid1/time/GetTimeMain.java
@@ -0,0 +1,28 @@
+package lecture.mid1.time;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalField;
+
+public class GetTimeMain {
+    public static void main(String[] args) {
+        LocalDateTime dt = LocalDateTime.of(2030, 1, 1, 13, 30, 59);
+        System.out.println("dt.get(ChronoField.YEAR) = " + dt.get(ChronoField.YEAR));
+        System.out.println("dt.get(ChronoField.MONTH_OF_YEAR) = " + dt.get(ChronoField.MONTH_OF_YEAR));
+        System.out.println("dt.get(ChronoField.DAY_OF_MONTH) = " + dt.get(ChronoField.DAY_OF_MONTH));
+        System.out.println("dt.get(ChronoField.HOUR_OF_DAY) = " + dt.get(ChronoField.HOUR_OF_DAY));
+        System.out.println("dt.get(ChronoField.MINUTE_OF_DAY) = " + dt.get(ChronoField.MINUTE_OF_HOUR));
+        System.out.println("dt.get(ChronoField.SECOND_OF_DAY) = " + dt.get(ChronoField.SECOND_OF_MINUTE));
+
+        System.out.println("dt.getYear() = " + dt.getYear());
+        System.out.println("dt.getMonth() = " + dt.getMonthValue());
+        System.out.println("dt.getDayOfMonth() = " + dt.getDayOfMonth());
+        System.out.println("dt.getHour() = " + dt.getHour());
+        System.out.println("dt.getMinute() = " + dt.getMinute());
+        System.out.println("dt.getSecond() = " + dt.getSecond());
+
+        System.out.println("dt.get(ChronoField.MINUTE_OF_DAY) = " + dt.get(ChronoField.MINUTE_OF_DAY));
+        System.out.println("dt.get(ChronoField.SECOND_OF_DAY) = " + dt.get(ChronoField.SECOND_OF_DAY));
+
+    }
+}
---
+++ src/lecture/mid1/time/LocalDateTimeMain.java
@@ -0,0 +1,32 @@
+package lecture.mid1.time;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class LocalDateTimeMain {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("now = " + now);
+
+        LocalDateTime ofDt = LocalDateTime.of(2016, 6, 16, 8, 10, 1);
+        System.out.println("ofDt = " + ofDt);
+
+        LocalDate localDate = ofDt.toLocalDate();
+        System.out.println("localDate = " + localDate);
+        LocalTime localTime = ofDt.toLocalTime();
+        System.out.println("localTime = " + localTime);
+
+        LocalDateTime localDateTime = LocalDateTime.of(localDate, localTime);
+        System.out.println("localDateTime = " + localDateTime);
+
+        LocalDateTime ofDtPlus = ofDt.plusDays(1000);
+        System.out.println("ofDtPlus = " + ofDtPlus);
+        ofDtPlus = ofDt.plusYears(1);
+        System.out.println("ofDtPlus = " + ofDtPlus);
+
+        System.out.println("now.isBefore(ofDt) = " + now.isBefore(ofDt));
+        System.out.println("now.isAfter(ofDt) = " + now.isAfter(ofDt));
+        System.out.println("now.isEqual(ofDt) = " + now.isEqual(ofDt));
+    }
+}
