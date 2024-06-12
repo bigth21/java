c07b0e6ad496f91e5e0b9db3bdf5d018881a9604
bigth21 <taekhyeon.nam@gmail.com>
2024-06-13 00:33:01 +0900

    Iterable, Iterator

---
+++ src/lecture/mid/collection/iterable/MyArrayMain.java
@@ -0,0 +1,18 @@
+package lecture.mid.collection.iterable;
+
+import java.util.Iterator;
+
+public class MyArrayMain {
+    public static void main(String[] args) {
+        var myArray = new MyArray(new int[]{1, 2, 3, 4});
+
+        var iterator = myArray.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        for (Integer value : myArray) {
+            System.out.println(value);
+        }
+    }
+}
