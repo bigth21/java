146dd92a5d844925a39de0bc9a4986c9d0ff660a
bigth21 <taekhyeon.nam@gmail.com>
2024-06-12 22:04:34 +0900

    Map

---
+++ src/lecture/mid/collection/map/JavaMapMain.java
@@ -0,0 +1,27 @@
+package lecture.mid.collection.map;
+
+import java.util.*;
+
+public class JavaMapMain {
+    public static void main(String[] args) {
+        run(new HashMap<>());
+        run(new LinkedHashMap<>());
+        run(new TreeMap<>());
+    }
+
+    private static void run(Map<String, Integer> map) {
+        System.out.println("map.getClass() = " + map.getClass());
+        map.put("C", 10);
+        map.put("B", 20);
+        map.put("A", 30);
+        map.put("1", 40);
+        map.put("2", 50);
+
+        Set<String> keys = map.keySet();
+        Iterator<String> iterator = keys.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next());
+        }
+        System.out.println();
+    }
+}
