310e870db74855dc496ecad5940661a31494993d
bigth21 <taekhyeon.nam@gmail.com>
2024-06-13 21:54:44 +0900

    Collections

---
+++ src/lecture/mid/collection/utils/CollectionsSortMain.java
@@ -0,0 +1,29 @@
+package lecture.mid.collection.utils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionsSortMain {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        Integer max = Collections.max(list);
+        System.out.println("max = " + max);
+        Integer min = Collections.min(list);
+        System.out.println("min = " + min);
+
+        Collections.shuffle(list);
+        System.out.println("list = " + list);
+        Collections.sort(list);
+        System.out.println("list = " + list);
+        Collections.reverse(list);
+        System.out.println("list = " + list);
+    }
+}
