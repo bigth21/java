67322dcfd7cfeb82353be3aa4806898ac695ac40
bigth21 <taekhyeon.nam@gmail.com>
2024-08-26 16:15:42 +0900

    Factorial

---
+++ src/algorithm/Factorial.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int factorial = factorial(5);
+        System.out.println(factorial);
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
