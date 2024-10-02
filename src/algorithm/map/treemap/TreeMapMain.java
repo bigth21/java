f0ad258a215aceb2b80092b684f04a7fc8655621
bigth21 <taekhyeon.nam@gmail.com>
2024-10-02 14:17:45 +0900

    Add usage of TreeMap-related APIs

---
+++ src/algorithm/map/treemap/TreeMapMain.java
@@ -0,0 +1,18 @@
+package algorithm.map.treemap;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeMapMain {
+    public static void main(String[] args) {
+        TreeMap<Integer, String> map = new TreeMap<>(); // Sorted according to the natural ordering of its keys
+        map.put(1, "First");
+        map.put(2, "Second");
+        map.put(3, "Third");
+        System.out.println("map = " + map);
+
+        System.out.println("map.firstEntry() = " + map.firstEntry());
+        System.out.println("map.lastEntry() = " + map.lastEntry());
+
+    }
+}
