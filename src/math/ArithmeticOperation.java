e40a2b8884fdf37b7f611079e9491f97a231efd0
bigth21 <taekhyeon.nam@gmail.com>
2024-10-04 14:25:09 +0900

    Add examples of arithmetic operations

---
+++ src/math/ArithmeticOperation.java
@@ -0,0 +1,51 @@
+package math;
+
+public class ArithmeticOperation {
+    public static void main(String[] args) {
+        // Addition
+        // term + term
+        // summand + summand
+        // addend + addend
+        // augend + addend
+        int sum = 1 + 2;
+        System.out.println("sum = " + sum);
+
+        // Subtraction
+        // term - term
+        // minuend - subtrahend
+        int subtraction = 5 - 3;
+        System.out.println("subtraction = " + subtraction);
+
+        // Multiplication
+        // factor x factor
+        // multiplier x multiplicand
+        int product = 3 * 4;
+        System.out.println("product = " + product);
+
+        // Division
+        // dividend / divisor
+        // numerator / denominator
+        int fraction = 12 / 3;
+        System.out.println("fraction = " + fraction);
+        int quotient = 12 / 3;
+        System.out.println("quotient = " + quotient);
+        int ratio = 12 / 3;
+        System.out.println("ratio = " + ratio);
+
+        // Exponentiation
+        // base ^ exponent
+        // base ^ power
+        double power = Math.pow(2, 3);
+        System.out.println("power = " + power);
+
+        // nth root
+        // degree root radicand
+        double root = Math.sqrt(2);
+        System.out.println("root = " + root);
+
+        // Logarithm
+        // log(base - anti logarithm)
+        double logarithm = Math.log(2);
+        System.out.println("logarithm = " + logarithm);
+    }
+}
