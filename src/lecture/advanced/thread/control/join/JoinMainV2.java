175e428a7aed66f6cb25f8d5f50098c52f706dce
bigth21 <taekhyeon.nam@gmail.com>
2024-10-06 19:19:25 +0900

    Add join with time out parameter example

---
+++ src/lecture/advanced/thread/control/join/JoinMainV2.java
@@ -0,0 +1,44 @@
+package lecture.advanced.thread.control.join;
+
+import static lecture.advanced.util.MyLogger.log;
+import static lecture.advanced.util.ThreadUtils.sleep;
+
+public class JoinMainV2 {
+    public static void main(String[] args) throws InterruptedException {
+        log("Start");
+
+        SumTask task1 = new SumTask(1, 50);
+        Thread thread1 = new Thread(task1, "thread-1");
+
+        thread1.start();
+
+        thread1.join(1000);
+
+        log("task1.result = " + task1.result);
+
+        log("End");
+    }
+
+    static class SumTask implements Runnable {
+        int startValue;
+        int endValue;
+        int result;
+
+        public SumTask(int startValue, int endValue) {
+            this.startValue = startValue;
+            this.endValue = endValue;
+        }
+
+        @Override
+        public void run() {
+            log("Start");
+            sleep(2000);
+            int sum = 0;
+            for (int i = startValue; i <= endValue; i++) {
+                sum += i;
+            }
+            result = sum;
+            log("End, result = " + result);
+        }
+    }
+}
