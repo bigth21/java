9e969f7bf58c3ef19a8df0f00ad66b0cfb2fe808
bigth21 <taekhyeon.nam@gmail.com>
2024-10-12 15:17:59 +0900

    Add volatile-related examples

---
+++ src/lecture/advanced/thread/volatiles/VolatileCountMain.java
@@ -0,0 +1,34 @@
+package lecture.advanced.thread.volatiles;
+
+import static lecture.advanced.util.MyLogger.log;
+import static lecture.advanced.util.ThreadUtils.sleep;
+
+public class VolatileCountMain {
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread thread = new Thread(task);
+        thread.start();
+
+        sleep(1_000);
+
+        task.flag = false;
+        log("flag = " + task.flag + ", count = " + task.count);
+
+    }
+
+    static class MyTask implements Runnable {
+        volatile boolean flag = true;
+        long count;
+
+        @Override
+        public void run() {
+            while (flag) {
+                count++;
+                if (count % 100_000_000 == 0) {
+                    log("flag = " + flag + ", count = " + count);
+                }
+            }
+            log("Terminated");
+        }
+    }
+}
