5c294b519a00fb299ac393bac342c45f3d35463e
bigth21 <taekhyeon.nam@gmail.com>
2024-10-23 12:22:07 +0900

    Add `producer-consumer-problems`-related example using ReentrantLock & Condition

--- src/lecture/advanced/thread/bounded/BoundedMain.java
+++ src/lecture/advanced/thread/bounded/BoundedMain.java
@@ -8,10 +8,10 @@ import static lecture.advanced.util.ThreadUtils.sleep;
 
 public class BoundedMain {
     public static void main(String[] args) {
-        BoundedQueue queue = new BoundedQueueV3(2);
+        BoundedQueue queue = new BoundedQueueV4(2);
 
-        producerFirst(queue);
-//        consumerFirst(queue);
+//        producerFirst(queue);
+        consumerFirst(queue);
     }
 
     private static void producerFirst(BoundedQueue queue) {
