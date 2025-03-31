6f20d8ff8b710ed5b98cbf5ad46af8ba84037036
bigth21 <taekhyeon.nam@gmail.com>
2025-04-01 08:20:03 +0900

    Polishing

--- src/string/StringBuilderBufferComparison.java
+++ src/string/StringBuilderBufferComparison.java
@@ -4,7 +4,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class StringBuilderBufferCompare {
+public class StringBuilderBufferComparison {
     public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 2; i++) { // To ensure the same conditions for optimization
             testStringBuilder();
